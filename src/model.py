46ad5801463392292fd45e429b41bf759b5c609e
nitinpathak84 <nitinpathak.84@gmail.com>
2026-02-23 18:16:44 +0530

    model.py

--- src/model.py
+++ src/model.py
@@ -3,10 +3,11 @@ from physicsnemo.models.mlp.fully_connected import FullyConnected
 
 
 def build_model(num_layers: int, layer_size: int, device):
-    # Input: (r,z) -> Output: T
-    return FullyConnected(
+    # Input: (r,z)  Output: T
+    model = FullyConnected(
         in_features=2,
         out_features=1,
         num_layers=num_layers,
         layer_size=layer_size,
-    ).to(device)
+    )
+    return model.to(device)
